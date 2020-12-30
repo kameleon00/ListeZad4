d32e46fda0a5a5b6d3d9f1de7b3780af99166ee0
Marko Djokic <marko1319@its.edu.rs>
2020-12-30 01:41:20 +0100

    Add project files.

---
+++ ListeZad4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> godine = new List<int>() { 2009, 2010, 2012 };
+            godine.Insert(2, 2011);
+            Console.WriteLine("Lista nakon umetanja: ");
+            foreach (int godina in godine)
+                Console.WriteLine(godina);
+            Console.WriteLine();
+            Console.WriteLine("===================================");
+
+            godine.InsertRange(godine.Count, new List<int>() { 2013, 2014, 2015 });
+            Console.WriteLine("Lista nakon inser range: ");
+            foreach (int godina in godine)
+                Console.WriteLine(godina);
+            Console.WriteLine();
+            Console.WriteLine("===================================");
+
+            Console.ReadLine();
+        }
+    }
+}
